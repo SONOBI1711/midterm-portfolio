365f26d68c91f19af764334ce79bb0578c78c0e0
CodeSandbox <support@codesandbox.io>
2026-01-28 20:09:43 +0000

    Sandbox snapshot commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
 import Button from "./Button";
+
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[68px]">
       <div className="max-w-[1240px] mx-auto h-full flex justify-between items-center">
         <div>
-          <p></p>
-          {"<SS /> "}
+          <p className="font-inter font-bold text-[30px] leading-[36px] tracking-[-2px] text-gray-900">
+            {"<SS /> "}
+          </p>
         </div>
         <div className="flex justify-center items-center">
           <ul className="list-none flex justify-around items-center space-x-[24px] mr-[48px]">
-            <li>About</li>
-            <li>Work</li>
-            <li>Testimonials</li>
-            <li>Contact</li>
+            <li className="font-inter font-medium text-[16px] leading-[24px] text-gray-600">
+              About
+            </li>
+            <li className="font-inter font-medium text-[16px] leading-[24px] text-gray-600">
+              Work
+            </li>
+            <li className="font-inter font-medium text-[16px] leading-[24px] text-gray-600">
+              Testimonials
+            </li>
+            <li className="font-inter font-medium text-[16px] leading-[24px] text-gray-600">
+              Contact
+            </li>
           </ul>
           <div className=" flex items-center justify-center space-x-[16px]">
             <img src="src/assets/Light-Dark.svg" alt="Light-Dark-icon" />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,12 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full h-[552px] mt-[68px] bg-red-500 py-[96px]">
+      <div className="max-w-[1240px] mx-auto h-[360px]">
+        <div className="text-section"></div>
+        <div className="image-section"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
