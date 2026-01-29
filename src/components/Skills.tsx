1b13626f73e9000118297b89df323a8c3a0ee821
CodeSandbox <support@codesandbox.io>
2026-01-29 14:02:08 +0000

    Sandbox snapshot commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,11 @@
 import Button from "./Button";
 import SectionTitle from "./SectionTitle";
+import SkillsLogo from "./SkillsLogo";
 
 export default function Skills() {
   return (
     <section className="w-full h-[560px] ">
-      <div className="max-w-[1280px] mx-auto py-[96px] px-[32px] flex flex-col items-center">
+      <div className="max-w-[1280px]  py-[96px] px-[32px] flex flex-col items-center">
         <div>
           <Button variant="secondary" size="sm" className="w-[75px] h-[28px]">
             Skills
@@ -13,7 +14,9 @@ export default function Skills() {
         <div>
           <SectionTitle text="The skills, tools and technologies I am really good at:"></SectionTitle>
         </div>
-        <div>component</div>
+        <div className="mt-12">
+          <SkillsLogo />
+        </div>
       </div>
     </section>
   );
