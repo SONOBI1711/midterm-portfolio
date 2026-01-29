051f79382a7430cce0b557103598af45d860d0ce
CodeSandbox <support@codesandbox.io>
2026-01-29 19:00:04 +0000

    Sandbox snapshot commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ import SkillsLogo from "./SkillsLogo";
 export default function Skills() {
   return (
     <section className="w-full h-[560px] ">
-      <div className="max-w-[1280px]  py-[96px] px-[32px] flex flex-col items-center">
+      <div className="max-w-[1280px] mx-auto py-[96px] px-[32px] flex flex-col items-center">
         <div>
           <Button variant="secondary" size="sm" className="w-[75px] h-[28px]">
             Skills
