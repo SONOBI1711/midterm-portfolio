6b1a3e31946fae52c77de0554aaba3d8282b0260
CodeSandbox <support@codesandbox.io>
2026-01-29 09:42:25 +0000

    Sandbox snapshot commit

---
+++ src/data/skills.ts
@@ -0,0 +1,20 @@
+export const skills = [
+    { icon: "/icons/js.svg", name: "Javascript" },
+    { icon: "/icons/ts.svg", name: "Typescript" },
+    { icon: "/icons/react.svg", name: "React" },
+    { icon: "/icons/next.svg", name: "Next.js" },
+    { icon: "/icons/node.svg", name: "Node.js" },
+    { icon: "/icons/express.svg", name: "Express.js" },
+    { icon: "/icons/nest.svg", name: "Nest.js" },
+    { icon: "/icons/socket.svg", name: "Socket.io" },
+  
+    { icon: "/icons/postgres.svg", name: "PostgreSQL" },
+    { icon: "/icons/mongo.svg", name: "MongoDB" },
+    { icon: "/icons/sass.svg", name: "Sass/Scss" },
+    { icon: "/icons/tailwind.svg", name: "Tailwindcss" },
+    { icon: "/icons/figma.svg", name: "Figma" },
+    { icon: "/icons/cypress.svg", name: "Cypress" },
+    { icon: "/icons/storybook.svg", name: "Storybook" },
+    { icon: "/icons/git.svg", name: "Git" },
+  ];
+  
