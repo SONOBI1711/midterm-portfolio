0c858fde714c6604c74cd30e832b0be72ff6e495
CodeSandbox <support@codesandbox.io>
2026-01-29 04:35:27 +0000

    Sandbox snapshot commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,28 @@
 import Avatar from "./Avatar";
+import Text from "./Text";
+import HeroLocation from "./HeroLocation";
+import HeroSocial from "./HeroSocial";
 
 const Hero = () => {
   return (
     <section className="w-full h-[552px] mt-[68px] py-[96px]">
-      <div className="max-w-[1240px] mx-auto h-[360px] flex justify-center items-center">
-        <div className="text-section"></div>
+      <div className="max-w-[1240px] mx-auto h-[360px] flex justify-between items-center">
+        <div className="text-section flex flex-col justify-start w-[720px] gap-[48px]">
+          <Text
+            variant="hero"
+            heading="Hi, I'm Sagar 👋"
+            paragraphs={[
+              "I am a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.",
+            ]}
+          />
+          <div>
+            <HeroLocation />
+          </div>
+
+          <div>
+            <HeroSocial />
+          </div>
+        </div>
         <div className="image-section">
           <Avatar
             src="src/assets/Hero-Avatar.png"
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,3 +1,40 @@
-export default function Text() {
-  return <></>;
-}
+const Text = ({
+  heading = "",
+  paragraphs = [],
+  list = [],
+  variant = "default",
+}) => {
+  const headingStyle =
+    variant === "hero"
+      ? "text-[60px] leading-[72px] font-bold"
+      : "text-[30px] leading-[36px] font-semibold";
+
+  const paragraphStyle = "text-[16px] leading-[24px] font-normal text-gray-600";
+
+  return (
+    <div className="flex flex-col gap-4">
+      {/* Heading */}
+      <h1 className={headingStyle}>{heading}</h1>
+
+      {/* Paragraphs */}
+      <div className="flex flex-col gap-4">
+        {paragraphs.map((text, index) => (
+          <p key={index} className={paragraphStyle}>
+            {text}
+          </p>
+        ))}
+      </div>
+
+      {/* List */}
+      {list.length > 0 && (
+        <ul className="list-disc pl-5 text-[16px] leading-[24px] text-gray-600">
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Text;
