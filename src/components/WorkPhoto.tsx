70ee8cab0bed5fc3f25a996d815a9b1c8e091a49
CodeSandbox <support@codesandbox.io>
2026-01-30 05:59:31 +0000

    Sandbox snapshot commit

--- src/components/WorkPhoto.tsx
+++ src/components/WorkPhoto.tsx
@@ -1,3 +1,17 @@
-export default function WorkPhoto({ photo: string }) {
-  return <div></div>;
-}
+type WorkPhotoData = {
+  src: string;
+  alt: string;
+  className: string;
+};
+
+const WorkPhoto = ({ src, alt, className }: WorkPhotoData) => {
+  return (
+    <div
+      className={`w-full max-w-[480px] aspect-[480/384] rounded-xl overflow-hidden ${className}`}
+    >
+      <img src={src} alt={alt} className="w-full h-full object-cover" />
+    </div>
+  );
+};
+
+export default WorkPhoto;
